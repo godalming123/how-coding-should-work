15ef9112d4641ff86513dac66fdaef10c724152e
godalming123 <68993177+godalming123@users.noreply.github.com>
2020-08-14 09:12:19 +0100

    update

--- my code to my binary.py
+++ my code to my binary.py
@@ -2,73 +2,12 @@ def MyCodeToMyMachine (code) :
   output = ""
   keyword = ""
   keywords = 0
-  state = [0]#this is the state say if we inside a function the state would be [f]
+  InFunction = []#this is the state say if we inside a function the state would be [f]
   for letter in list (code) :
-    if letter == ";" :
-      if keyword == "+" :
-        output += "10000"
-        state += "f"
-      elif keyword == "-" :
-        output += "01000"
-        state += "f"
-      elif keyword == "*" :
-        output += "11000"
-        state += "f"
-      elif keyword == "/" :
-        output += "00100"
-        state += "f"
-      elif keyword == "^" :
-        output += "10100"
-        state += "f"
-      elif keyword == "SquareRoot" :
-        output += "01100"
-        state += "f"
-      elif keyword == "get" :
-        output += "11100"
-        state += "f"
-      elif keyword == "input" :
-        output += "00010"
-        state += "f"
-      elif keyword == "str" :
-        output += "10010"
-        state += "f"
-      elif keyword == "MultiLineStr" :
-        output += "01010"
-        state += "f"
-      elif keyword == "mapping" :
-        output += "11010"
-        state += "f"
-      elif keyword == "Int" :
-        output += "00110"
-        state += "f"
-      elif keyword == "float" :
-        output += "00001"
-        state += "f"
-      elif keyword == "list" :
-        output += "10001"
-        state += "f"
-      elif keyword == "run" :
-        output += "01001"
-        state += "f"
-      elif keyword == "RunIf" :
-        output += "11001"
-        state += "f"
+    if letter == ";" :#char to end function
       output += "00000"
-    
-    elif letter == " " :
-      keyword = ""
-      keywords += 1#there has been a keyword passed in so we add one to the keywords var
-    
-    elif letter == "n" :#n means to create a new line witch is 00000 in my code
-      output += "000000"
-      keyword = ""
-      keywords = 0
-    
-    elif state[0] == "f" :
-      output += "{10000:b}".format(ord (letter))
-    
-    else :
-      keyword += letter
+      InFunction.pop (0)
+    elif letter == " " :#char to initialise function
       if keywords == 0 :
         if keyword == "set" :
           output += "100"
@@ -78,6 +17,69 @@ def MyCodeToMyMachine (code) :
           output += "110"
         elif keyword == "RunIf" :
           output += "001"
+      
+      elif keyword == "+" :
+        output += "10000"
+        InFunction.append ("f")
+      elif keyword == "-" :
+        output += "01000"
+        InFunction.append ("f")
+      elif keyword == "*" :
+        output += "11000"
+        InFunction.append ("f")
+      elif keyword == "/" :
+        output += "00100"
+        InFunction.append ("f")
+      elif keyword == "^" :
+        output += "10100"
+        InFunction.append ("f")
+      elif keyword == "SquareRoot" :
+        output += "01100"
+        InFunction.append ("f")
+      elif keyword == "get" :
+        output += "11100"
+        InFunction.append ("f")
+      elif keyword == "input" :
+        output += "00010"
+        InFunction.append ("f")
+      elif keyword == "str" :
+        output += "10010"
+        InFunction.append ("f")
+      elif keyword == "MultiLineStr" :
+        output += "01010"
+        InFunction.append ("f")
+      elif keyword == "mapping" :
+        output += "11010"
+        InFunction.append ("f")
+      elif keyword == "Int" :
+        output += "00110"
+        InFunction.append ("f")
+      elif keyword == "float" :
+        output += "00001"
+        InFunction.append ("f")
+      elif keyword == "list" :
+        output += "10001"
+        InFunction.append ("f")
+      elif keyword == "run" :
+        output += "01001"
+        InFunction.append ("f")
+      elif keyword == "RunIf" :
+        output += "11001"
+        InFunction.append ("f")
+      keyword = ""
+      keywords += 1#there has been a keyword passed in so we add one to the keywords var
+    
+    elif letter == "n" :#n means to create a new line witch is 00000 in my code
+      output += "000000"
+      keyword = ""
+      keywords = 0
+    
+    elif InFunction != [] :
+      output += format(ord(letter), 'b')
+    
+    else :
+      keyword += letter
+      
   return output
 def MyCodeToBinaryFile (FileToConvert) :
   with open (FileToConvert, "r") as filecontents :
